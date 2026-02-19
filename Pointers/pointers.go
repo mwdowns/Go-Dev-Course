ce37f648bd841dd3330f4f782d8d1464f6509e45
Matthew Downs <22185242+mwdowns@users.noreply.github.com>
2026-02-19 12:39:25 -0500

    added comments

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 	age := 32
-	fmt.Println(age)
+	fmt.Println(age) // should be 32
 	fmt.Println("Adult years: ", getAdultYears(&age))
-	fmt.Println(age)
+	fmt.Println(age) // should be 14
 }
 
 func getAdultYears(age *int) int {
